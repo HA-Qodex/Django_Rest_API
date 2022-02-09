31cc4c14d91ced3d6c8978052b4f306e5c01d59a
Huzaifa Ahmed <huzaifa.ahmed33@gmail.com>
2022-02-09 18:06:18 +0600

    initial commit

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import *
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('products/', ProductView.as_view()),
-    path('favourite/', FavouriteView.as_view())
+    path('favourite/', FavouriteView.as_view()),
+    path('cart/', CartView.as_view()),
+    path('order/', OrderView.as_view()),
+    path('login/', obtain_auth_token),
+    path('registration/', RegisterView.as_view()),
 ]
--- shop/views.py
+++ shop/views.py
@@ -37,3 +37,47 @@ class FavouriteView(APIView):
         except:
             response_message = {'error': True}
         return Response(response_message)
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializers = UserSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response({"error": serializers.default_error_messages})
+        return Response({"error": serializers.errors})
+
+
+class CartView(APIView):
+    permission_classes = [IsAuthenticated, ]
+    authentication_classes = [TokenAuthentication, ]
+
+    def get(self, request):
+        user = request.user
+        try:
+            cart_obj = Cart.objects.filter(user=user).filter(isComplete=False)
+            data = []
+            cart_serializer = CartSerializer(cart_obj, many=True)
+            for cart in cart_serializer.data:
+                cart_product_obj = CartProduct.objects.filter(cart=cart["id"])
+                cart_product_obj_serializer = CartProductSerializer(cart_product_obj, many=True)
+                cart['cartProducts'] = cart_product_obj_serializer.data
+                data.append(cart)
+            response_message = {"error": False, "data": data}
+        except:
+            response_message = {"error": False, "data": "No Data"}
+        return Response(response_message)
+
+
+class OrderView(APIView):
+    permission_classes = [IsAuthenticated,]
+    authentication_classes = [TokenAuthentication,]
+
+    def get(self, request):
+        try:
+            query = Order.objects.filter(cart__user=request.user)
+            serializer = OrderSerializer(query, many=True)
+            response_message = {"error": False, "data": serializer.data}
+        except:
+            response_message = {"error": True, "data": "No Data"}
+        return Response(response_message)
